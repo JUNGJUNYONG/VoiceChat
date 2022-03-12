97100b6fae4d7b8a289264d6ee86b0a4509122df
정준용 <jkouese@gmail.com>
2022-03-13 02:20:06 +0900

    작업분

--- src/sidebar/Search/Search.tsx
+++ src/sidebar/Search/Search.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {ChangeEventHandler} from 'react';
 import './Search.css'
 
-export default class Search extends React.Component<unknown, unknown> {
+export default class Search extends React.Component<{onChange: ChangeEventHandler}, unknown> {
+    constructor(props: {onChange: ChangeEventHandler}) {
+        super(props);
+    }
+
 
     render() {
         return (
@@ -12,7 +16,7 @@ export default class Search extends React.Component<unknown, unknown> {
                     <path fill="currentColor"
                           d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                 </svg>
-                <input className={"Search_input"}  />
+                <input className={"Search_input"}  onChange={this.props.onChange} />
             </div>
         );
     }
--- src/sidebar/SideBar.tsx
+++ src/sidebar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import './SideBar.css'
 import Search from "./Search/Search";
 import ConversationList from "./ConversationList/ConversationList";
@@ -14,28 +14,39 @@ enum ICON {
     default = 'http://photo.hankooki.com/newsphoto/2010/01/19/alba03201001191522340.jpg'
 }
 
-export default class SideBar extends React.Component<unknown, { RoomList: Array<RoomListType> }> {
+export default class SideBar extends React.Component<unknown, { renderList: Array<RoomListType> }> {
 
     constructor(props: unknown) {
         super(props);
         this.state = {
-            RoomList: [{name: 'test1',icon: ICON.default},{name: 'test2',icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNpTHC8ac6uBrRtR59nhWU8gc4U7APeN-KIQ&usqp=CAU"}]
+            renderList: [],
         }
+
     }
+    private roomList = [{name: 'test1',icon: ICON.default},{name: 'test2',icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNpTHC8ac6uBrRtR59nhWU8gc4U7APeN-KIQ&usqp=CAU"}];
+
+    componentDidMount(){
+        this.setState({renderList: this.roomList});
+    }
+
 
     private _clickHandler(e: MouseEvent) {
         console.log('click!!!',e);
+    }
+    private searchOnChange(e: ChangeEvent){
+        const searchText =(e.target as HTMLInputElement).value;
+        this.setState({renderList: this.roomList.filter((Room)=>{ return Room.name.includes(searchText)})})
     }
 
 
     render() {
         return (
             <div className="SideBar">
-                <Search/>
+                <Search onChange={this.searchOnChange.bind(this)}/>
                 <ConversationList>
-                    {this.state.RoomList.map((data,key) => {
+                    {this.state.renderList.map((data,key) => {
                         return (
-                            <ConversationItem onClick={this._clickHandler} key={key}>
+                            <ConversationItem onClick={this._clickHandler.bind(this)} key={key}>
                                 <div className={"SideBarImgItem"}>
                                     <img
                                         src={data.icon}/>
